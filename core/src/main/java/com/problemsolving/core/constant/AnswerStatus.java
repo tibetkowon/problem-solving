57c406937b9258cdba7eeb3d307ef90acc61958e
고원 <kowon@gowon-ui-Macmini.local>
2026-03-28 01:05:08 +0900

    feat(core): add ProblemType and AnswerStatus enums

---
+++ core/src/main/java/com/problemsolving/core/constant/AnswerStatus.java
@@ -0,0 +1,7 @@
+package com.problemsolving.core.constant;
+
+public enum AnswerStatus {
+    CORRECT,   // 정답
+    PARTIAL,   // 부분 정답
+    WRONG      // 오답
+}
---
+++ core/src/main/java/com/problemsolving/core/constant/ProblemType.java
@@ -0,0 +1,7 @@
+package com.problemsolving.core.constant;
+
+public enum ProblemType {
+    OBJECTIVE_SINGLE,    // 객관식 (단일 정답)
+    OBJECTIVE_MULTIPLE,  // 객관식 (복수 정답)
+    SUBJECTIVE           // 주관식
+}
